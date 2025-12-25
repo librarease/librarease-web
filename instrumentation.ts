4f8afa84ea8e55f2d29bca924f3f8b5728786b9c
ammo <solidifyarmor@gmail.com>
2025-12-26 00:18:45 +0630

    fix: propagate tracing context headers

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,5 +3,10 @@ import { registerOTel } from '@vercel/otel'
 export function register() {
   registerOTel({
     serviceName: 'librarease-next',
+    instrumentationConfig: {
+      fetch: {
+        propagateContextUrls: [process.env.API_URL!],
+      },
+    },
   })
 }
